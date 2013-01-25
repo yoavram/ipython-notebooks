4226d962d5ecdda489310af080ffd070895d6d2d
Yoav Ram <yoavram+github@gmail.com>
2013-01-25 17:27:05 +0200

    moved some code to auxiliary py file

---
+++ simulation_frameworks_aux.py
@@ -0,0 +1,29 @@
+from matplotlib.pyplot import *
+
+def plot_summary(num_balls, means, stds, game_name):
+    figure(figsize=(15,5))
+    subplot(121)
+    plot(num_balls, means,'ro')
+    xlabel("# balls each color")
+    ylabel("avg. # draws")
+    title(game_name + " - average")
+    subplot(122)
+    plot(num_balls, stds,'bo')
+    xlabel("# balls each color")
+    ylabel("# draws")
+    title(game_name + " standard deviation")
+
+def plot_histograms(results, n_bins = 25, legend_title= "# balls each"):
+    figure(figsize=(18,5))
+    subplot(131)
+    for i,dataset in enumerate(results[0:3,]):
+        hist(dataset, bins=n_bins, label=str(num_balls[i]))
+    legend(title=legend_title);
+    subplot(132)
+    for i,dataset in enumerate(results[3:6,]):
+        hist(dataset, bins=n_bins, label=str(num_balls[i+3]))
+    legend(title=legend_title);
+    subplot(133)
+    for i,dataset in enumerate(results[6:,]):
+        hist(dataset, bins=n_bins, label=str(num_balls[i+6]))
+    legend(title=legend_title)
